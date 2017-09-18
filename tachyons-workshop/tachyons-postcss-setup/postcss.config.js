003dd13064b8c08799372c0ae6098232c37b05e2
Andrew MacMurray <a.macmurray@icloud.com>
2017-09-18 16:56:58 +0100

    move postcss clean import last

--- tachyons-workshop/tachyons-postcss-setup/postcss.config.js
+++ tachyons-workshop/tachyons-postcss-setup/postcss.config.js
@@ -6,7 +6,7 @@ module.exports = {
     require('postcss-import'), // allows you to use @import
     require('autoprefixer'), // adds vendor prefixes like -webkit or -moz to your css for better support
     require('postcss-custom-media'), // can set media queries as variables
-    require('postcss-clean'), // minifies the output css (i.e. removes all the spaces and comments)
-    require('postcss-custom-properties') // allows you to use css variables e.g. var(--my-variable)
+    require('postcss-custom-properties'), // allows you to use css variables e.g. var(--my-variable)
+    require('postcss-clean') // minifies the output css (i.e. removes all the spaces and comments)
   ]
 }
